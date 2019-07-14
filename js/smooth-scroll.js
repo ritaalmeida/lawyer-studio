3327facb73c8c173a1dd25997896b63762647dc5
Filipe Mendes <filipemendes@Filipes-MacBook-Pro.local>
2019-07-14 04:42:48 +0100

    navbar init

--- js/smooth-scroll.js
+++ js/smooth-scroll.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
 
-  // Add smooth scrolling to all links
-  $('a.home__arrow').on('click', function(event) {
+  function smoothScroll(event) {
     // Make sure this.hash has a value before overriding default behavior
     if (this.hash && this.hash !== "") {
       // Prevent default anchor click behavior
@@ -13,12 +12,17 @@ $(document).ready(function() {
       // Using jQuery's animate() method to add smooth page scroll
       // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
       $('html, body').animate({
-        scrollTop: $(hash).offset().top
+        scrollTop: $(hash).offset().top - 16
       }, 800, function(){
 
         // Add hash (#) to URL when done scrolling (default click behavior)
         window.location.hash = hash;
       });
     } // End if
-  });
+  }
+
+  // Add smooth scrolling to all links
+  $('a.home__arrow').on('click', smoothScroll, event);
+  $('a.navbar-item').on('click', smoothScroll, event);
+
 });
